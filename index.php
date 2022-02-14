5970d0f501d8381fd21a6c282a4896324acd4b42
HRahman1777 <hasibur.cse7@gmail.com>
2022-02-14 15:32:51 +0600

    form select added

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ include "db_conn.php";
 $sql_query = "SELECT * FROM users";
 $result = mysqli_query($connection, $sql_query);
 
-$limit_per_page = 50;
+$limit_per_page = getLimit();
+$select_options_value = [10, 30, 50, 80, 100];
 
 $total_number_of_data = mysqli_num_rows($result);
 $total_page_need = ceil($total_number_of_data / $limit_per_page);
@@ -16,15 +17,15 @@ $final_data = dataPerPage($connection, $start_limit, $limit_per_page);
 
 
 
-function getPageNum($total_page_need)
+function getPageNum($tpn)
 {
     if (isset($_GET['pageNo'])) {
         $p = $_GET['pageNo'];
 
         if ($p < 1) {
             $p = 1;
-        } elseif ($p > $total_page_need) {
-            $p = $total_page_need;
+        } elseif ($p > $tpn) {
+            $p = $tpn;
         }
     } else {
         $p = 1;
@@ -33,15 +34,31 @@ function getPageNum($total_page_need)
     return $p;
 }
 
-function setDistabled($page, $total_page_need)
+function getLimit()
 {
-    if ($page > 1) {
+    if (isset($_POST['set_limit'])) {
+        $page_limit = $_POST['set_limit'];
+    } else {
+
+        if (isset($_GET['limit'])) {
+            $page_limit = $_GET['limit'];
+        } else {
+            $page_limit = 30;
+        }
+    }
+
+    return $page_limit;
+}
+
+function setDistabled($p, $tpn)
+{
+    if ($p > 1) {
         $active_pre = '';
     } else {
         $active_pre = 'disabled';
     }
 
-    if ($page < $total_page_need) {
+    if ($p < $tpn) {
         $active_next = '';
     } else {
         $active_next = 'disabled';
@@ -88,13 +105,30 @@ function dataDetailsPerPage($td, $st, $lpp)
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <script src="script.js"></script>
+
 </head>
 
 <body>
 
     <div class="container">
+
         <h3 class="text-center">A Custom Pagination - User Details</h3>
         <hr>
+
+        <form action="index.php" method="POST" class="form-limit">
+
+            <select class="form-select" name="set_limit" onchange="this.form.submit()">
+                <?php
+                for ($i = 0; $i < count($select_options_value); $i++) {
+                    $limit_per_page == $select_options_value[$i] ? $select_check = "Selected" : $select_check = "";
+
+                    echo "<option $select_check value='$select_options_value[$i]'> $select_options_value[$i] </option>";
+                }
+                ?>
+            </select>
+
+        </form>
 
         <p>
             <?php
@@ -131,9 +165,8 @@ function dataDetailsPerPage($td, $st, $lpp)
                 <?php
                 $disable = setDistabled($page, $total_page_need);
                 ?>
-
                 <li class="page-item <?php echo $disable[0]; ?>">
-                    <a class="page-link" href="index.php?pageNo=<?php echo $page - 1; ?>" tabindex="-1" aria-disabled="true">Previous</a>
+                    <a class="page-link" href="index.php?pageNo=<?php echo $page - 1 . "&limit=$limit_per_page"; ?>" tabindex="-1" aria-disabled="true">Previous</a>
                 </li>
 
                 <?php
@@ -145,12 +178,12 @@ function dataDetailsPerPage($td, $st, $lpp)
                         $active_page_number = '';
                     }
                 ?>
-                    <li class="page-item <?php echo $active_page_number; ?>"><a class="page-link " href="index.php?pageNo=<?php echo $loop_page; ?>"> <?php echo $loop_page; ?> </a></li>
+                    <li class="page-item <?php echo $active_page_number; ?>"><a class="page-link " href="index.php?pageNo=<?php echo $loop_page . "&limit=$limit_per_page"; ?>"> <?php echo $loop_page; ?> </a></li>
 
                 <?php }; ?>
 
                 <li class="page-item <?php echo $disable[1]; ?>">
-                    <a class="page-link" href="index.php?pageNo=<?php echo $page + 1; ?>">Next</a>
+                    <a class="page-link" href="index.php?pageNo=<?php echo $page + 1 . "&limit=$limit_per_page"; ?>">Next</a>
                 </li>
 
             </ul>
